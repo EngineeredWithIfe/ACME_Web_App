cc597d2fa3335ae0124181d07b58cde107882190
Ife <45706096+EngineeredWithIfe@users.noreply.github.com>
2024-07-18 15:18:01 -0400

    Update AddCustomerForm.cs
    Pass customer list when button is clicked

--- AddCustomerForm.cs
+++ AddCustomerForm.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using System.Windows.Forms;
 
 namespace ACME_Web_App
@@ -32,16 +24,11 @@ namespace ACME_Web_App
             customerManager.AddCustomer(customer);
         }
 
-        private void nameTextBox_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
             this.Hide();
-            CustomerPortal customerPortal = new CustomerPortal();
-            customerPortal.Show();
+            EditCustomerForm editCustomerForm = new EditCustomerForm(customerManager.GetCustomers());
+            editCustomerForm.Show();
         }
     }
 }
